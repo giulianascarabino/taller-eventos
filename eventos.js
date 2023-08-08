9c674d028a9c93c7edd0d5a49b35c10730a2407a
giulianascarabino <limada321@gmail.com>
2023-08-08 15:47:40 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
 let eventoDiv= document.getElementById("divicion");
- 
+ let button= document.getElementById("boton");
+
 eventoDiv.addEventListener("click", function(){
 alert("Hola! Soy el div");
 
+})
+button.addEventListener("click",function(event){
+    event.stopPropagation();
 })
 
 })
