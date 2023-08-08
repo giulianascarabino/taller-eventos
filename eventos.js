60678bd685c0f4ae8871c662c69277f3d3898282
giulianascarabino <limada321@gmail.com>
2023-08-08 15:19:42 -0300

    se agrega alerta div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+let eventoDiv= document.getElementById("divicion");
+ 
+eventoDiv.addEventListener("click", function(){
+alert("Hola! Soy el div");
+
+})
+
+})
